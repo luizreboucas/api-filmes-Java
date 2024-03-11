cd7eb5c82f40d8abbe12fafefdb9038c23526b46
luiz.reboucas <luiz.reboucas@hostweb.com.br>
2024-03-11 13:55:06 -0300

    alteração funcionalidade pesquisa filme

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,40 +1,69 @@
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+        var opcao = 0;
+        while (opcao != 3) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Bem vindo ao IMDBBase, a seleção dos melhores filmes de todos os tempos");
+            System.out.println("o que deseja fazer?");
+            System.out.println("[1] -> pesquisar filme\n[2] -> listar os melhores 10 filmes\n[3] -> sair");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
+            String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+            ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-         ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-        // String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/NASA-APOD.json";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+            var http = new ClienteHttp();
+            String json = http.buscaDados(url);
 
-        var http = new ClienteHttp();
-        String json = http.buscaDados(url);
+            // exibir e manipular os dados
+            List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
-        // exibir e manipular os dados 
-        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+            var geradora = new GeradoraDeFigurinhas();
+            if(opcao ==1){
+                System.out.println("Digite o filme o qual deseja pesquisar. obs: nome em inglês, Pascal Case com espacos ");
+                String nomePesquisa = scanner.nextLine();
+                List<Conteudo> busca = conteudos
+                        .stream()
+                        .filter(filme -> filme.getTitulo().equals(nomePesquisa))
+                                .collect(Collectors.toList());
 
-        var geradora = new GeradoraDeFigurinhas();
+                if(!busca.isEmpty()){
+                    var resultado = busca.get(0);
+                    System.out.println("O filme " + resultado.getTitulo() + " está presente na lista dos 500 melhores filmes do mundo\no poster deste filme foi gerado e está na pasta ./saída");
+                    InputStream inputStream = new URL(resultado.getUrlImagem()).openStream();
+                    String nomeArquivo = "C:\\Users\\luiz.reboucas\\Documents\\api-filmes-Java\\alura-stickers\\saida/" + resultado.getTitulo() + ".png";
+                    geradora.cria(inputStream, nomeArquivo);
+                    break;
+                }
+                else{
+                    System.out.println("filme não encontrado");
+                }
+            }
+            if(opcao == 2) {
+                for (int i = 0; i < 9; i++) {
 
-        for (int i = 0; i < 3; i++) {
+                    Conteudo conteudo = conteudos.get(i);
 
-            Conteudo conteudo = conteudos.get(i);
+                    InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+                    String nomeArquivo = "C:\\Users\\luiz.reboucas\\Documents\\api-filmes-Java\\alura-stickers\\saida/" + conteudo.getTitulo() + ".png";
 
-            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nomeArquivo = "C://NovoWorkspace/Projetos/imersao-java-aula3/alura-stickers/saida/" + conteudo.getTitulo() + ".png";
+                    geradora.cria(inputStream, nomeArquivo);
 
-            geradora.cria(inputStream, nomeArquivo);
-
-            System.out.println(conteudo.getTitulo());
-            System.out.println();
+                    System.out.println(conteudo.getTitulo());
+                    System.out.println();
+                }
+            }
+            if(opcao < 0 || opcao > 3){
+                System.out.println("opção inválida!");
+            }
         }
     }
 }
